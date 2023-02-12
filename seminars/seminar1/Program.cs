8febd3319faa066f813458484bab6588529c7fb8
Svetlana Arutsiunova <ys20051980@gmail.com>
2023-02-12 12:25:39 +0300

    добавили семинар 1

---
+++ seminars/seminar1/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задача 1. Вывести квадрат числа
+
+//Console.WriteLine("Input number");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int quad = num * num;
+//Console.WriteLine($"Quad of {num} is {quad}");
+    
+//Задача 2 Принимает на вход 2 числа и проверяет, является ли первое квадратом второго
+//Console.WriteLine("Input first number");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input second number");
+//int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+//int quad = num2 * num2;
+
+//if (quad == num1)
+//{ 
+//    Console.WriteLine("First number is quad of second number");
+//}
+
+//else 
+//{
+//    Console.WriteLine("First number is not quad of second number");
+//}
+
+// 3aдача 3 от отрицательного к положительному
+
+//Console.WriteLine("Input number");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//int current = num * (-1);
+
+//while (current <= num)
+//{
+   // Console.Write(current + " ");
+   // current ++;
+//} 
+
+
+// 3aдача 3-1 от положительного k отрицательному
+
+//Console.WriteLine("Input number");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//int current = num * (-1);
+
+//while (current <= num)
+//{
+//    Console.Write(num + " ");
+ //   num --;
+//} 
+
+//Задача 4 написать программу, которая принимает на вход трехзначное число, а на выходе показывает последнюю цифру числа
+
+Console.WriteLine("Input three-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+
+Console.WriteLine($"last digit of {num} is {result}");
+
+
+
+ 
+
+
+
+
+
